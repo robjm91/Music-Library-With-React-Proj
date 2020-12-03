9b7a5ff43b4818a15425198596b5c8d59e37dcf2
Robert J Magaña <robjmagana@gmail.com>
2020-12-03 15:31:05 -0800

    created repo.js to import axios. also updated app.js to test the link to the server

--- musiclibrary/src/app.js
+++ musiclibrary/src/app.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-
-
-class App extends React.Component{
-   render() {
-       return (
-            <h1>Music Library With React</h1>
-       );
-   } 
+// Class component structure #2 --------------
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
 }
+ 
+ export default App;
 
-export default App;
+
+
+
+
+
+// Class Component structure #1 ------------
+// class App extends React.Component{
+//    render() {
+//        return (
+//             <h1>Music Library With React</h1>
+//        );
+//    } 
+// }
+
+// export default App;
--- musiclibrary/src/index.js
+++ musiclibrary/src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
+
+
+  //Class Component structure #2 ------------
+  ReactDOM.render(<App/>, document.getElementById("root"));
 
 
 
-ReactDOM.render(
-    <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
-  </div>,
-  document.getElementById("root")
-  );
-
-  
+  // Functional Way --------
   // import React from "react";
   // import ReactDOM from "react-dom";
-  // import App from "./app";
   
-  // ReactDOM.render(<App/>, document.getElementById("root"));
+  // ReactDOM.render(
+  //     <div>
+  //     <h1>Hello World!</h1>
+  //     <p>This is a paragraph.</p>
+  //   </div>,
+  //   document.getElementById("root")
+  //   );
