ec777a6c7c0618c57a0e6076481b0546de1584bd
Robert J Magaña <robjmagana@gmail.com>
2020-12-02 16:30:35 -0800

    began constructing class components

--- musiclibrary/src/app.js
+++ musiclibrary/src/app.js
@@ -1,1 +1,12 @@
+import { ReactComponent } from '*.svg';
 import React from 'react';
+
+class App extends React.Component{
+   render() {
+       return (
+            <h1>Music Library With React</h1>
+       );
+   } 
+}
+
+export default App;
--- musiclibrary/src/index.js
+++ musiclibrary/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 
-const element = <h1>Hello World</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(</App>, document.getElementById("root"));
