75c2fd127194478fec9c1efb2cf223d94c133ae0
Robert J Magaña <robjmagana@gmail.com>
2020-12-03 16:20:31 -0800

    imported axios and used constructor method to console log api data

--- musiclibrary/src/app.js
+++ musiclibrary/src/app.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://www.devcodecampmusiclibrary.com/api/music"
+})
+
+//Class Component structure #3 ------------
+class App extends React.Component{ 
+    constructor() {
+        super();
+        api.get("/").then(res => {
+            console.log(res.data);
+        })
+    }
+   render() {
+       return (
+            <h1>Music Library With React</h1>
+       );
+   } 
+}
+
+export default App;
+
+
+
+
 
 // Class component structure #2 --------------
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        }
-    }
-}
- 
- export default App;
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
 
-
-
-
-
-
-// Class Component structure #1 ------------
-// class App extends React.Component{
-//    render() {
-//        return (
-//             <h1>Music Library With React</h1>
-//        );
-//    } 
+//         }
+//     }
 // }
+ 
+//  export default App;
 
-// export default App;
+
+// Class Component #1 ------------
+// class App extends React.Component{
+//     render() {
+//         return (
+//              <h1>Music Library With React</h1>
+//         );
+//     } 
+//  }
+ 
+//  export default App;
--- musiclibrary/src/repo.js
+++ musiclibrary/src/repo.js
@@ -1,1 +1,7 @@
 import axios from "axios"
+
+ let url = "http://www.devcodecampmusiclibrary.com/api/music";
+
+ export function getMusicAll() {
+     return axios.get("${url}/music")
+ }
